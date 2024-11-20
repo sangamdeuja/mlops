64529493d07a21fe8f7e10bbdf7e6b3b32bf4749
sangam deuja <sangamad1@gmail.com>
2024-11-20 10:16:47 +0200

    add permission to write login token

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -26,6 +26,7 @@ module "storage_account" {
   storage_account_name = var.storage_account_name
   resource_group_name  = var.resource_group_name
   location             = var.location
+  depends_on           = [module.resource_group]
 }
 
 module "application_insight" {
@@ -33,6 +34,7 @@ module "application_insight" {
   application_insights_name = var.application_insights_name
   resource_group_name       = var.resource_group_name
   location                  = var.location
+  depends_on                = [module.resource_group]
 }
 
 module "key_vault" {
@@ -40,6 +42,7 @@ module "key_vault" {
   key_vault_name      = var.key_vault_name
   resource_group_name = var.resource_group_name
   location            = var.location
+  depends_on          = [module.resource_group]
 }
 
 module "mlworkspace" {
@@ -50,5 +53,6 @@ module "mlworkspace" {
   application_insights_id = module.application_insight.application_insights_id
   key_vault_id            = module.key_vault.key_vault_id
   storage_account_id      = module.storage_account.storage_account_id
+  depends_on              = [module.resource_group]
 
 }
